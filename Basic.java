8b045f666ef841cceccb78bd0a8c014469b4e498
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2021-09-08 22:51:33 +0600

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,24 @@
+
+
+public class Basic{
+	public static void main(String args[]){
+		int i = 1234;
+		char c = 'a';
+		String str = "Kazi Sourav";
+		short s = 32677;
+		float f = 15.101f;
+		double d = 10.3250000126;
+		boolean b = false;
+		
+		
+		System.out.println("int i = "+i);
+		System.out.println("char c = "+c);
+		System.out.println("short a = "+s);
+		System.out.println("String str = "+str); 
+		System.out.println("float f = "+f);
+		System.out.println("double d = "+d);
+		System.out.println("boolean b = "+b);
+		//System.out.println("Kazi Sourav \n01585225 \nBsc.in CSE");
+		//System.out.print("\"018523215\"");
+	}
+}
